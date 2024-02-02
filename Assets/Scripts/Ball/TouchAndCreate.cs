c51f0129d27976584a87534105d469792c5cd9ac
LEEJH <totolikid@naver.com>
2024-02-02 23:40:52 +0900

    바나나 제작중

--- Assets/Scripts/Ball/TouchAndCreate.cs
+++ Assets/Scripts/Ball/TouchAndCreate.cs
@@ -1,18 +1,41 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchAndCreate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject createObj;
+
+    bool isDestroyed = false;
+
+    ContactFilter2D filter;
+
+
     void Start()
     {
-        
+        isDestroyed = false;
+
+        filter = new ContactFilter2D();
+        //filter.SetLayerMask()
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.isTrigger) return;//충돌 가능 오브젝트들과 만 충돌하기
+        if (collision.gameObject.CompareTag("Player")) return;//충돌 된게 플레이어이면 무시하기
+
+        if (isDestroyed) return;
+        isDestroyed = true;
+
+        Instantiate(createObj, transform.position, Quaternion.identity);
+
+        Destroy(this.gameObject);
+
     }
 }
---
+++ Assets/Scripts/Mines/DebuffMine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuffMine : MonoBehaviour
+{
+    [SerializeField] float damageAmount = 10f;
+
+    [Header("Debuff Setting")]
+    [SerializeField] float frictionAmount = 0.2f;
+
+    [SerializeField, Space(5f)] float debuffTime = 3f;
+
+    PhysicsMaterial2D phMat;
+
+    bool isUsed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isUsed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isUsed) return;
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ApplyDebuff(collision.gameObject.GetComponent<Player>());
+        }
+    }
+
+    void ApplyDebuff(Player player)
+    {
+        if (isUsed) return;
+        isUsed = true;
+
+        player.TakeDamage(damageAmount);
+
+        //이후 디버프 기능 들어가면 디버프 추가
+        phMat = player.GetComponent<Rigidbody2D>().sharedMaterial;
+
+        phMat.friction *= frictionAmount;
+
+        Invoke(nameof(UnApplyDebuff), debuffTime);
+    }
+
+    void UnApplyDebuff()
+    {
+        phMat.friction /= frictionAmount;
+
+        Destroy(this.gameObject);
+    }
+}
