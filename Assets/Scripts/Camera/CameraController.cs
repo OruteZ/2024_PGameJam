519ca3fdf24c56b8c862e7fdff044d68d03bc8b8
LEEJH <totolikid@naver.com>
2024-02-03 18:30:04 +0900

    카메라 이동 및 배경화면 연출 적용

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [System.Serializable]
+    class CamOffset
+    {
+        public float camSizeOffset;
+
+        public Vector3 camPosOffset;
+    }
+
+    [SerializeField]
+    CamOffset firstOffset;
+
+    [SerializeField, Space(5f)]
+    CamOffset lastOffset;
+
+    [SerializeField, Space(5f)]
+    float delayTime;
+
+    float _time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.orthographicSize = firstOffset.camSizeOffset;
+
+        this.transform.position = firstOffset.camPosOffset;
+
+        _time = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Camera.main.orthographicSize = Mathf.Lerp(firstOffset.camSizeOffset,lastOffset.camSizeOffset,_time/delayTime);
+
+        this.transform.position = Vector3.Lerp(firstOffset.camPosOffset, lastOffset.camPosOffset, _time / delayTime);
+
+        _time += Time.deltaTime;
+        if (_time >= delayTime) _time = delayTime;
+        
+    }
+}
