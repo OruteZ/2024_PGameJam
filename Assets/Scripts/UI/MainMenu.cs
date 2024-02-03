59310ee3ad26e8585cfd82b5ae1c3ea4225c669b
LEEJH <totolikid@naver.com>
2024-02-04 07:12:24 +0900

    가장 문제였던 해상도로 인한 UI 이상해지는 문제 해결

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 using Utility.Manager;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
     private float moveDistance;
 
+    [SerializeField] List<GameObject> startObj = new List<GameObject>();
+    [SerializeField] float startDelay = 3f;
+
+    [SerializeField, Space(5f)] GameObject NonUIObj;
+
     private void Start()
     {
+        foreach (GameObject obj in startObj) obj.SetActive(false);
         Invoke(nameof(PlayBGM), 1f);
+        Invoke(nameof(StartObj), startDelay);
+    }
+
+    void StartObj()
+    {
+        foreach (GameObject obj in startObj) obj.SetActive(true);
     }
 
     private void PlayBGM()
@@ -47,6 +60,7 @@ public class MainMenu : MonoBehaviour
     private void PlaySoundAndMove(int direction)
     {
         SoundManager.Instance.PlaySFX("menu-select");
-        transform.position += Vector3.left * (moveDistance * direction);
+        transform.position += Vector3.left * (moveDistance * direction * Screen.width / 1920);
+        NonUIObj.transform.position += Vector3.left * (moveDistance * direction * Screen.width / 1920);
     }
 }
