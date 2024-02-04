bf0519c45ac9e2086f549924f6f9dd6203b11089
LEEJH <totolikid@naver.com>
2024-02-04 12:54:24 +0900

    궁극기 충전 아이템 추가 완료

---
+++ Assets/Scripts/Item/Consumed Item/UltGaugePack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UltGaugePack : ConsumedItemObj 
+{
+    [SerializeField,Range(0f,1f)] float ultAmount = 0.5f;
+
+    protected override void ConsumeItem()
+    {
+        base.ConsumeItem();
+
+        usingPlayer.ultimateGauge += ultAmount;
+        //플레이어에게 힐은 준다
+
+        DestroyGameObj();
+    }
+}
